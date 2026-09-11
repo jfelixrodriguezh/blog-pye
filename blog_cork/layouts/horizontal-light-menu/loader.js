6834c58d9ff089d050a841ac02c62a0d70c26e2f
Jose Felix Rodriguez <jfelixrodriguez@gmail.com>
2026-09-11 11:06:12 +0200

    prototipo

---
+++ blog_cork/layouts/horizontal-light-menu/loader.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", function(){
+
+    // Remove Loader
+    var load_screen = document.getElementById("load_screen");
+    document.body.removeChild(load_screen);
+    
+});
+
